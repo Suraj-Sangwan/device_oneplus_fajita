f0b8dd1039f752b11a4e59e0fbe8b68e6dce8ba1
Suraj Sangwan <76832120+Suraj-Sangwan@users.noreply.github.com>
2021-06-09 20:54:23 +0530

    Added Octavi Stuff

--- octavi_fajita.mk
+++ octavi_fajita.mk
@@ -32,6 +32,13 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 
+# OctaviOS stuff
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_WANTS_FOD_ANIMATIONS := true
+OCTAVI_BUILD_TYPE := UNOFFICIAL
+OCTAVI_DEVICE_MAINTAINER := Suraj
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := octavi_fajita
 PRODUCT_DEVICE := fajita
